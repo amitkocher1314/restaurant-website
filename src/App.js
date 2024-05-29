5c26094dcbffbc6f585b3e4240294bfec0ff7205
amitkocher1314 <amitkocher7206251314@gmail.com>
2024-05-30 01:22:46 +0530

    Meal-summary&Available-meal

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { Fragment } from "react";
 import Header from "./components/Layout/Header"
+import Meal from "./components/Meals/Meal";
 function App() {
   return (
+    <Fragment>
     <Header />
+    <main>
+    <Meal />
+    </main>
+    </Fragment>
   )
 }
 
